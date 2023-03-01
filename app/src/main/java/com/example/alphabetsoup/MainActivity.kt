e6974a1be0d9b874e912f768b05823a32b9c1dcc
Pol Renalias <prenaliasp@inscastellet.cat>
2023-03-01 18:16:22 +0100

    Revert "Firebase deps added (2)"
    This reverts commit cc842b9784e7fc1ec4bf5a1a5aa9799779cbf1b0.

--- app/src/main/java/com/example/alphabetsoup/MainActivity.kt
+++ app/src/main/java/com/example/alphabetsoup/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.alphabetsoup
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,13 +24,13 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        val BTMLOGIN = findViewById<Button>(R.id.BTMLOGIN)
-        val BTMREGISTRO = findViewById<Button>(R.id.BTMREGISTRO)
+        var BTMLOGIN = findViewById<Button>(R.id.BTMLOGIN);
+        var BTMREGISTRO = findViewById<Button>(R.id.BTMREGISTRO);
         BTMLOGIN.setOnClickListener(){
-            Toast.makeText(this, "click botó login",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "click botó login",Toast.LENGTH_LONG).show();
         }
         BTMREGISTRO.setOnClickListener(){
-            Toast.makeText(this, "click botó Registre",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "click botó Registre",Toast.LENGTH_LONG).show();
         }
     }
 
