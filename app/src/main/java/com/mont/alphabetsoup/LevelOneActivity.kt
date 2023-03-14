c17fd6f508255e1026b8378df0c63d0d09b6fb55
Pol Renalias <prenaliasp@inscastellet.cat>
2023-03-14 19:34:18 +0100

    Erased merge annotations

--- app/src/main/java/com/mont/alphabetsoup/LevelOneActivity.kt
+++ app/src/main/java/com/mont/alphabetsoup/LevelOneActivity.kt
@@ -1,30 +1,65 @@
 package com.mont.alphabetsoup
 
+import android.R.attr.button
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Gravity
 import android.view.View
-import android.widget.*
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 
 class LevelOneActivity : AppCompatActivity() {
-    @SuppressLint("ResourceType")
-    val b: BoardController = BoardController()
+    lateinit var v00: ImageView
+    lateinit var countTime: TextView
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level_one)
-
-        val tableRows = findViewById<View>(R.id.board) as TableLayout
-
-        for (y in 0 until tableRows.childCount) {
-            val row: TableRow = tableRows.getChildAt(y) as TableRow
-
-            for (x in 0 until row.childCount) {
-                val cell: TextView = findViewById(row.getChildAt(x).id)
-                cell.setOnClickListener(){
-                    b.cellStatusChange(y, x)
+        var letterPanel: GridLayout = findViewById<GridLayout>(R.id.letterPanel)
+        countTime = findViewById(R.id.timerText)
+        startTimeCounter()
+        for (x in 0..5) {
+            for (y in 0..7) {
+                var l: Letter = Letter(this)
+                l.setColor()
+                val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+                    GridLayout.LayoutParams.WRAP_CONTENT,
+                    GridLayout.LayoutParams.WRAP_CONTENT
+                )
+                l.layoutParams = params
+                l.gravity = Gravity.CENTER_HORIZONTAL
+                l.text = "X"
+                l.id = x
+                letterPanel.addView(l)
+                l.setOnClickListener {
+                    l.setColorRed()
                 }
             }
         }
+
+/*        val var00 :ImageView = findViewById(R.id.rect00);
+        var00.setOnClickListener{
+            Toast.makeText(this, "Tocat el: "+var00.id, Toast.LENGTH_LONG).show()
+        }*/
+    }
+
+    private fun startTimeCounter() {
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                countTime.text = (millisUntilFinished / 1000).toString()
+            }
+
+            override fun onFinish() {
+                countTime.text = "S'ha acabat el temps"
+            }
+        }.start()
     }
 }
